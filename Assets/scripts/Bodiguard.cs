82899567d331a252f5a74a76f6db114375027ede
larusso94 <matarroxp@gmail.com>
2018-11-24 16:05:25 +0100

    Bodiguards

---
+++ Assets/scripts/Bodiguard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bodiguard : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider collision)
+	{
+		if (collision.gameObject.CompareTag ("Enemy")) {
+			Destroy (collision.gameObject);
+		}
+	}
+
+}
