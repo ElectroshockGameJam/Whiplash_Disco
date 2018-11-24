a7e25c765ab3519bfe7c4ab2b42aa6dc426dee9b
AleixBV <aleixborrellvives@gmail.com>
2018-11-24 18:44:05 +0100

    create billboard script

---
+++ Assets/scripts/Billboard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+    public Transform target;
+
+    int timer = 10;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        timer--;
+        if(timer <= 0)
+        {
+            transform.LookAt(target);
+        }
+	}
+}
