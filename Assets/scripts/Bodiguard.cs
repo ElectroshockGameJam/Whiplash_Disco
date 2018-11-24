3417e15212506225ebb30d3ea6346245830f64cf
Jordi Pascual <jor.pas.fon@gmail.com>
2018-11-25 00:22:41 +0100

    master: add waves

--- Assets/scripts/Bodiguard.cs
+++ Assets/scripts/Bodiguard.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Bodiguard : MonoBehaviour {
 
+    public GameManager gameManager;
+
 	private void OnTriggerEnter(Collider collision)
 	{
 		if (collision.gameObject.CompareTag ("Enemy")) {
-			Destroy (collision.gameObject);
+            gameManager.killPlayer();
+            Destroy (collision.gameObject);
 			ScoreManager.scoreManager.addCoins();
 			ScoreManager.scoreManager.addPoint();
 		}
--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,57 +1,115 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
-	public Transform[] respawnPsitions;
-	public Rigidbody [] enemys;
+public class GameManager : MonoBehaviour
+{
+
+    public Text waveText;
+    public Transform[] respawnPsitions;
+    public Rigidbody[] enemys;
 
 
     public Transform Player;
-	public float speed;
-	public float factor;
+    public float speed;
+    public float factor;
 
-	public float countdown;
+    public float countdown;
 
-	private float  diff, counterEnemys;
-	// Use this for initialization
-	void Start () {
-		
-		diff = 0;
-		counterEnemys = 0;
-	}
+    private int actualLevel;
+    private enum WaveStatus { prepare, preparing, spawn, spawning, waitCompletion };
+    private WaveStatus waveState;
+    private int enemiesToEndWave, counterEnemys;
 
-	// Update is called once per frame
-	void Update () {
-		diff -= Time.deltaTime ;
+    private float diff;
+    // Use this for initialization
+    void Start()
+    {
+        diff = 0;
+        counterEnemys = 0;
+        actualLevel = 1;
+        waveState = WaveStatus.spawn;
+    }
 
-		//Debug.Log ("Countdown: " + diff);
+    // Update is called once per frame
+    void Update()
+    {
+        switch (waveState)
+        {
+            case WaveStatus.prepare:
+                waveText.text = "Prepare for the next wave...";
+                StartCoroutine(PrepareWave());
+                actualLevel++;
+                waveState = WaveStatus.preparing;
+                break;
+            case WaveStatus.preparing:
+                break;
+            case WaveStatus.spawn:
+                StartCoroutine(SpawnWave());
+                waveState = WaveStatus.spawning;
+                break;
+            case WaveStatus.spawning:
+                break;
+            case WaveStatus.waitCompletion:
+                if (enemiesToEndWave <= 0)
+                {
+                    ScoreManager.scoreManager.addCoins();
+                    ScoreManager.scoreManager.addPoint();
+                    waveState = WaveStatus.prepare;
+                }
+                break;
+        }
+    }
 
-		if ((int)diff <= 0) {
-			
-			Debug.Log ("Create enemy");
+    private IEnumerator PrepareWave()
+    {
+        yield return new WaitForSeconds(2.0f);
+        waveText.text = "";
+        yield return new WaitForSeconds(3.0f);
+        int i = 3;
+        while (i > 0)
+        {
+            waveText.text = i.ToString();
+            yield return new WaitForSeconds(1.0f);
+            i--;
+        }
+        waveText.text = "";
+        waveState = WaveStatus.spawn;
+    }
 
-			counterEnemys++;
+    private IEnumerator SpawnWave()
+    {
+        enemiesToEndWave = actualLevel * 2 + 8;
+        int enemiesToSpawn = enemiesToEndWave;
+        int spawned = 0;
+        while (spawned < enemiesToSpawn) { 
+            Debug.Log("Create enemy");
 
-			countdown *= 0.99f;
-			diff = countdown;
+            counterEnemys++;
 
-			Debug.Log ("New Diff: " + diff);
+            Debug.Log("New Diff: " + diff);
 
-			int transfInd = Random.Range(0, respawnPsitions.Length);
-			int objectInd = Random.Range(0, enemys.Length);
+            int transfInd = Random.Range(0, respawnPsitions.Length);
+            int objectInd = Random.Range(0, enemys.Length);
 
-			Rigidbody obj = (Rigidbody) Instantiate (enemys[objectInd], respawnPsitions [transfInd].position, Quaternion.identity);
+            Rigidbody obj = (Rigidbody)Instantiate(enemys[objectInd], respawnPsitions[transfInd].position, Quaternion.identity);
             obj.gameObject.GetComponent<MoveTo>().goal = Player;
             obj.isKinematic = true;
-		
 
-			obj.name = " "+Time.deltaTime;
 
-			obj.gameObject.SetActive(true);
-				
-		
+            obj.name = " " + Time.deltaTime;
 
-		}
-	}
+            obj.gameObject.SetActive(true);
+            yield return new WaitForSeconds(2.0f);
+            spawned++;
+        }
+
+        waveState = WaveStatus.waitCompletion;
+    }
+
+    public void killPlayer()
+    {
+        enemiesToEndWave--;
+    }
 }
