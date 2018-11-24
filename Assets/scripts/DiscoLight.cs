0b64da2cbe8e83f058e78ff35f3b9d2bd6bbddc9
AleixBV <aleixborrellvives@gmail.com>
2018-11-24 18:31:35 +0100

    add audio and light change

--- Assets/scripts/DiscoLight.cs
+++ Assets/scripts/DiscoLight.cs
@@ -6,13 +6,19 @@ public class DiscoLight : MonoBehaviour {
 
     public List<Color> colorsList;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    float timer = 60.0f / 175.0f;
+    float countdown = 0.0f;
 	
 	// Update is called once per frame
 	void Update () {
-		
+        countdown -= Time.deltaTime;
+
+        if(countdown <= 0.0f)
+        {
+            int random = Random.Range(0, colorsList.Count);
+            GetComponent<Light>().color = colorsList[random];
+
+            countdown = timer;
+        }
 	}
 }
