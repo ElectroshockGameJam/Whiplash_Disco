14c84c8fe10ce6280ea721ef6efcb936000f3a45
Cristofol Dauden <cristofol.dauden@estudiants.urv.cat>
2018-11-24 01:15:16 +0100

    Toful: player movement

---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public float speed;
+	// Use this for initialization
+	void Start ( ) {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.GetKey("left") || Input.GetKey( KeyCode.A ) ){
+			gameObject.transform.Translate( -speed * Time.deltaTime, 0, 0 );
+		}
+		if( Input.GetKey("right") || Input.GetKey( KeyCode.D ) ){
+			gameObject.transform.Translate( speed * Time.deltaTime, 0, 0 );
+		}
+		if( Input.GetKey("up") || Input.GetKey( KeyCode.W ) ){
+			gameObject.transform.Translate( 0, 0, speed * Time.deltaTime );
+		}
+		if( Input.GetKey("down") || Input.GetKey( KeyCode.S ) ){
+			gameObject.transform.Translate( 0, 0, -speed * Time.deltaTime );
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Whiplash Disco
 
+Game developed during the Game Jam carried out in Tarragona 11/18
+
+
+## Authors
+
+* **Cristòfol Daudén Esmel** - [toful](https://github.com/toful)
+* **Oussama El Azizi** - [OussamaElazizi](https://github.com/OussamaElazizi)
+* **Aleix Borrell Vives** - [AleixBV](https://github.com/AleixBV)
