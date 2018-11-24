6d867a8857b425b5e5211e5fb4bb69eb1d2eb464
Oussama El Azizi <oussama.elazizi@estudiants.urv.cat>
2018-11-24 13:25:44 +0100

    respawn y move using mesh

---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public Transform[] respawnPsitions;
+	public Rigidbody [] enemys;
+
+
+	public float speed;
+	public float factor;
+
+	public float countdown;
+
+	private float  diff, counterEnemys;
+	// Use this for initialization
+	void Start () {
+		
+		diff = countdown;
+		counterEnemys = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		diff -= Time.deltaTime ;
+
+		//Debug.Log ("Countdown: " + diff);
+
+		if ((int)diff <= 0) {
+			
+			Debug.Log ("Create enemy");
+
+			counterEnemys++;
+
+			countdown *= 0.95f;
+			diff = countdown;
+
+			Debug.Log ("New Diff: " + diff);
+
+			int transfInd = Random.Range (0, 100) % respawnPsitions.Length;
+			int objectInd = Random.Range (0, 100) % enemys.Length;
+
+			Rigidbody obj = (Rigidbody) Instantiate (enemys[objectInd], respawnPsitions [transfInd].position, Quaternion.identity);
+			obj.isKinematic = true;
+		
+
+			obj.name = " "+Time.deltaTime;
+
+			obj.gameObject.SetActive(true);
+				
+		
+
+		}
+	}
+}
---
+++ Assets/scripts/MoveTo.cs
@@ -0,0 +1,44 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+
+	public Transform goal;
+	private NavMeshAgent agent;
+	public int counter;
+	private int diffCounter;
+
+	void Start(){
+		agent = GetComponent<NavMeshAgent> ();
+		diffCounter = counter;
+	}
+
+	void FixedUpdate()
+	{
+		
+		diffCounter--;
+		if (diffCounter <= 0) {
+			goal = GameObject.FindGameObjectWithTag ("objective").gameObject.transform;
+
+			agent.SetDestination (goal.position);
+			diffCounter = counter ;
+		}
+	}
+
+
+
+
+	/*void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Sphere") {
+
+			Destroy (gameObject);
+
+		}
+	}*/
+
+
+}
