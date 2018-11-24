d7733870ccc92cc994f58722a102199cbb67c7d2
Jordi Pascual <jor.pas.fon@gmail.com>
2018-11-24 18:54:32 +0100

    master: add Pause Menu

---
+++ Assets/scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour {
+
+    public Button resume;
+    public Button exit;
+    public Canvas canvas;
+
+	// Use this for initialization
+	void Start () {
+        resume.onClick.AddListener(Resume);
+        exit.onClick.AddListener(Exit);
+        canvas.gameObject.SetActive(false);
+	}
+
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
+            } else
+            {
+                Resume();
+            }
+            
+        }    
+    }
+
+    private void Resume()
+    {
+        canvas.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    private void Exit()
+    {
+        Application.Quit();
+    }
+}
