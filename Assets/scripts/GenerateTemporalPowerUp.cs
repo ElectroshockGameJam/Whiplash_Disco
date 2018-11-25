95687d21af884624db5486c8a38c2d99299976e3
Cristofol Dauden <cristofol.dauden@estudiants.urv.cat>
2018-11-25 09:46:20 +0100

    barman position

--- Assets/scripts/GenerateTemporalPowerUp.cs
+++ Assets/scripts/GenerateTemporalPowerUp.cs
@@ -6,6 +6,7 @@ public class GenerateTemporalPowerUp : MonoBehaviour {
 
     [HideInInspector] public Collider powerUp;
     [HideInInspector] public GameManager gameManager;
+    private bool spawned = false;
 
     private void OnTriggerEnter(Collider collision)
     {
@@ -13,8 +14,11 @@ public class GenerateTemporalPowerUp : MonoBehaviour {
             float x = Random.Range( -10, 10 );
             float z = Random.Range( -10, 10 );
             Vector3 position = new Vector3( x, 3, z );
-            Collider obj = (Collider) Instantiate ( powerUp, position, Quaternion.identity );
-            obj.gameObject.GetComponent<ControllTemporalPowerUp>().gameManager = gameManager;
+            if( !spawned ){
+                Collider obj = (Collider) Instantiate ( powerUp, position, Quaternion.identity );
+                obj.gameObject.GetComponent<ControllTemporalPowerUp>().gameManager = gameManager;
+                spawned = true;
+            }
             Destroy( gameObject );
         }
     }
