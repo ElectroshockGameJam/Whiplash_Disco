a3d9638be138f16708d5d7793546407ea7e54816
Cristofol Dauden <cristofol.dauden@estudiants.urv.cat>
2018-11-25 02:10:56 +0100

    Tòful: auto generating power ups when killing an enemy

--- Assets/scripts/Bodiguard.cs
+++ Assets/scripts/Bodiguard.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Bodiguard : MonoBehaviour {
 
+    public Collider powerUpGenerator;
+    public Collider powerUp;
+
 	private void OnTriggerEnter(Collider collision)
 	{
 		if (collision.gameObject.CompareTag ("Enemy")) {
 			Destroy (collision.gameObject);
 			ScoreManager.scoreManager.addCoins();
 			ScoreManager.scoreManager.addPoint();
+
+            if ( Random.Range( 0, 6 ) == 0 ) generatePowerUp();
 		}
-	}
+    }
+
+    private void generatePowerUp(){
+        float x = Random.Range( -10, 10 );
+        float z = Random.Range( -10, 10 );
+        Vector3 position = new Vector3( x, 4, z );
+        Collider obj = (Collider) Instantiate ( powerUpGenerator, position, Quaternion.identity );
+        obj.gameObject.GetComponent<GenerateTemporalPowerUp>().powerUp = powerUp;
+    }
 
 }
---
+++ Assets/scripts/GenerateTemporalPowerUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTemporalPowerUp : MonoBehaviour {
+
+    public Collider powerUp;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.CompareTag ("Player")) {
+            float x = Random.Range( -10, 10 );
+            float z = Random.Range( -10, 10 );
+            Vector3 position = new Vector3( x, 3, z );
+            Instantiate ( powerUp, position, Quaternion.identity );
+            Destroy( gameObject );
+        }
+    }
+}
