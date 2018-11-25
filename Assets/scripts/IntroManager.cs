af9abc21e06a3594f8a64b5af4e423ea579b68d9
AleixBV <aleixborrellvives@gmail.com>
2018-11-25 12:55:57 +0100

    Finish intro scene

--- Assets/scripts/IntroManager.cs
+++ Assets/scripts/IntroManager.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class IntroManager : MonoBehaviour {
-    
+
+    public GameObject LoadingCanvas;
+    public GameObject MenuCanvas;
+
+    private void Start()
+    {
+        LoadingCanvas.SetActive(false);
+    }
+
     public void  StartGame()
     {
+        LoadingCanvas.SetActive(true);
+        MenuCanvas.SetActive(false);
         SceneManager.LoadScene("main_scene");//(1);
     }
 
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 }
