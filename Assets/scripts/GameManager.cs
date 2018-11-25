8a1b200cba4734cd442bdd11567ec2248744b570
Jordi Pascual <jor.pas.fon@gmail.com>
2018-11-25 01:21:28 +0100

    master: add infinite music

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-
     public Text waveText;
     public Transform[] respawnPsitions;
     public Rigidbody[] enemys;
---
+++ Assets/scripts/PlayMusic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour {
+
+    public AudioClip[] audios;
+
+	void Start()
+    {
+        StartCoroutine(playRandom());
+    }
+
+    private IEnumerator playRandom()
+    {
+        for (; ; ) {
+            this.gameObject.GetComponent<AudioSource>().clip = audios[Random.Range(0, audios.Length)];
+            this.gameObject.GetComponent<AudioSource>().Play();
+            yield return new WaitForSeconds(this.gameObject.GetComponent<AudioSource>().clip.length);
+        }
+    }
+}
